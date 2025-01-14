551c3cc40e1cb52a29fb4695733d46a513662824
matheusgomes2020 <matheusgomez2013@gmail.com>
2025-01-14 13:10:01 -0300

    final

--- app/src/main/java/com/matheus/aulafrasesdodia/MainActivity.java
+++ app/src/main/java/com/matheus/aulafrasesdodia/MainActivity.java
@@ -1,6 +1,8 @@
 package com.matheus.aulafrasesdodia;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    String[] frases = {
+            "Frase 1",
+            "Frase 2",
+            "Frase 3"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,4 +31,29 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void gerarFrase( View view ) {
+
+        TextView texto = findViewById( R.id.textResultado );
+        //int numeroAleatorio = (int) ( Math.random() * frases.length );
+        int numeroAleatorio = new Random().nextInt(3);
+        String frase = frases[numeroAleatorio];
+
+        texto.setText( frase );
+
+    }
+
+    public void exibirTodas( View view ) {
+
+        TextView texto = findViewById( R.id.textResultado );
+
+        String textoResultado = "";
+        for ( String frase: frases ) {
+            textoResultado = textoResultado + frase + "\n";
+        }
+
+        texto.setText( textoResultado );
+
+    }
+
 }
